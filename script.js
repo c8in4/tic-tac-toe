3b30dc99e47af9f117085689085eed8fc1c61db4
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-07-26 22:40:21 +1200

    Add gameBoard with functions to console.log it

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('hello world')
+const gameBoard = (() => {
+  const rows = 3;
+  const columns = 3;
+  let board = [];
+
+  for (i = 0; i < rows; i++) {
+    board[i] = [];
+    for (j = 0; j < columns; j++) {
+      board[i].push('-');
+    };
+  };
+
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    const boardCells = board.map((row) => row.map((cell) => cell));
+    console.log(boardCells);
+  };
+
+return { getBoard, printBoard };
+
+})();
+
+
+gameBoard.getBoard();
+
+
+const game = {};
+
+const createPlayer = (name) => {
+
+  return {};
+};
+
